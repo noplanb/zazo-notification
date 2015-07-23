5a875fdd6c2c792b87449d73046b986c6d60a8cd
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-07-23 21:28:57 +0300

    Disable active_record and action_mailer (temp)

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails'
 # Pick the frameworks you want:
 require 'active_model/railtie'
 # require "active_job/railtie"
-require 'active_record/railtie'
+# require 'active_record/railtie'
 require 'action_controller/railtie'
-require 'action_mailer/railtie'
+# require 'action_mailer/railtie'
 # require 'action_view/railtie'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -30,6 +30,6 @@ module ZazoNotification
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
